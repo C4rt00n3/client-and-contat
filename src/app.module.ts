1f6fb5a3fb8a7c131414639122f29da90e7c6216
C4rt00n3 <rafaelcruzpaes@gmail.com>
2023-05-28 13:34:50 -0300

    feat:modelUsers

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ClientModule } from './module/client/client.module';
 import { ContactModule } from './module/contact/contact.module';
 import { AuthModule } from './module/auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ClientModule, ContactModule, AuthModule],
+  imports: [ClientModule, ContactModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
