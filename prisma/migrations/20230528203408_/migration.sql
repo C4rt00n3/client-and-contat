f5d12c0e313ba2e11ed77bad7dc778b004d78eab
C4rt00n3 <rafaelcruzpaes@gmail.com>
2023-05-31 18:03:00 -0300

    feat/Docker

--- prisma/migrations/20230528203408_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `img_user_src` to the `user` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "user" ADD COLUMN     "img_user_src" TEXT NOT NULL;
