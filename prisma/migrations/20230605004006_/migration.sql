1fde93b7813c41f8b2e895608700bca8001ed5f9
C4rt00n3 <rafaelcruzpaes@gmail.com>
2023-06-08 16:21:11 -0300

    fix/routes

--- prisma/migrations/20230605004006_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "client" DROP CONSTRAINT "client_userId_fkey";
-
--- AddForeignKey
-ALTER TABLE "client" ADD CONSTRAINT "client_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
